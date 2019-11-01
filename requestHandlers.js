babe70163530890691ce2b28630acb69dbc9acba
Toropon <duck7quick@gmail.com>
2019-11-01 19:07:11 +0900

    POSTデータの扱い方の仕上げ完了

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>' +
@@ -10,7 +10,7 @@ function start(response) {
         '</head>' +
         '<body>' +
         '<form action="/upload" method="post">' +
-        '<textarea name="trext" rows="20" cols="60"></textarea>' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
         '<input type="submit" value="Submit text" />' +
         '</form>' +
         '</body>' +
@@ -21,10 +21,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello Upload");
+    response.write("You've sent the text:" + querystring.parse(postData).text);
     response.end();
 }
 
--- router.js
+++ router.js
@@ -23,11 +23,12 @@ exports.start = start;
 exports.upload = upload;
 */
 
-
-function route(handle, pathname, response) {
+// /uploadページで、受け取った内容を表示します。
+// postDataをリクエストハンドラに渡す必要がある
+function route(handle, pathname, response, postData) {
     console.log("About to route a request for" + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response);
+        handle[pathname](response, postData);
     } else {
         console.log("No request handler found for" + pathname);
         response.writeHead(404, { "Content-Type": "text/plain" });
