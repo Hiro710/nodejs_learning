959b4c88f7d924dfc3a9cc380246c832a0812632
Toropon <duck7quick@gmail.com>
2019-11-05 21:26:40 +0900

    delete postData, add request

--- router.js
+++ router.js
@@ -1,37 +1,13 @@
-/*
-function route(pathname) {
-	console.log("About to route a request for" + pathname);
-}
-
-exports.route = route;
-
-*/
-
-
-/*
-// ルータにルーティングする先のものを渡すことで、
-// リクエストハンドラとルータを繋ぎ合わせることができる
-function start() {
-    console.log("Request handler 'start' was called.");
-}
-
-function upload() {
-    console.log("Request handler 'upload' was called.");
-}
-
-exports.start = start;
-exports.upload = upload;
-*/
-
 // /uploadページで、受け取った内容を表示します。
 // postDataをリクエストハンドラに渡す必要がある
-function route(handle, pathname, response, postData) {
+// postDataはもういらないので、代わりにrequestを渡します
+function route(handle, pathname, response, request) {
     console.log("About to route a request for" + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response, postData);
+        handle[pathname](response, request);
     } else {
         console.log("No request handler found for" + pathname);
-        response.writeHead(404, { "Content-Type": "text/plain" });
+        response.writeHead(404, { "Content-Type": "text/html" });
         response.write("404 Not found");
         response.end();
     }
--- server.js
+++ server.js
@@ -4,27 +4,13 @@ var url = require("url");
 // ルーティングの為の関数を引数で指定
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
+        // postDataを扱う部分と (node-formidableが直接取り扱うことになる)
+        // request.setEncodingの行を削除
+        // 代わりにrequestをルータに渡す
         var pathname = url.parse(request.url).pathname;
         console.log("Request for" + pathname + "received.");
-
-        // 受信データのエンコーディングをUTF-8と定義
-        request.setEncoding("utf8");
-
-        // 新しいPOSTデータの塊を受信する度にpostData変数に追記していく”data”イベントリスナーを追加
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '" +
-                postDataChunk + "'.");
-        });
-
-        // 全てのPOSTデータが収集できた時にだけ呼び出されるよう
-        // ルータ呼び出し部分をendイベントコールバック内に移動
-        request.addListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, postData);
     }
-
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
 }
